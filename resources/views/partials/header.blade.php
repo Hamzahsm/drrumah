dd112fa09e66f2c50a85e7701baf4cedb981f575
Hamzah SM <hamzahsaiful782@gmail.com>
2024-04-03 09:09:02 +0700

    modifikasi navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -16,27 +16,79 @@
                 @guest
                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                     <li class="nav-item">
-                        <a href="{{ route('login') }}" class="nav-link text-white"><i class="bi bi-person"></i> Login</a>
+                        <a href="/" class="nav-link menu-warning"><i class="bi bi-record"></i> Home</a>
                     </li>
                     <hr>
                     <li class="nav-item">
-                        <a href="{{ route('register') }}" class="nav-link text-white"><i class="bi bi-box-arrow-in-right"></i> Register</a>
+                        <a href="#" class="nav-link menu-warning"><i class="bi bi-record"></i> Tentang Kami</a>
                     </li>
                     <hr>
-                    {{-- <li class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Jasa Desain
+                    <li class="nav-item">
+                        <a class="nav-link dropdown-toggle menu-warning" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-record"></i> Layanan Kami 
                         </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#" class="dropdown-item">Rumah</a></li>
-                            <li><a href="#" class="dropdown-item">Hotel</a></li>
-                        </ul>
-                    </li> --}} 
+                          <ul class="dropdown-menu"> 
+                            <li><a class="dropdown-item menu-hover p-3 border-bottom" href="#"><i class="bi bi-arrow-right-short"></i> Jasa Arsitek</a></li>
+
+                            <li><a class="dropdown-item menu-hover p-3 border-bottom" href="#"><i class="bi bi-arrow-right-short"></i> Jasa Bangun Rumah</a></li>
+
+                            <li><a href="#" class="dropdown-item menu-hover p-3 border-bottom"><i class="bi bi-arrow-right-short"></i> Jasa Bangun Kantor</a></li>
+
+                            <li><a href="#" class="dropdown-item menu-hover p-3 border-bottom"><i class="bi bi-arrow-right-short"></i> Jasa Bangun Gudang</a></li>
+
+                            <li><a href="#" class="dropdown-item menu-hover p-3 border-bottom"><i class="bi bi-arrow-right-short"></i> Jasa Bangun Ruko</a></li>
+
+                            <li><a href="#" class="dropdown-item menu-hover p-3 border-bottom"><i class="bi bi-arrow-right-short"></i> Jasa Bangun Pengurusan PBG</a></li>
+
+                            <li><a href="#" class="dropdown-item menu-hover p-3 border-bottom"><i class="bi bi-arrow-right-short"></i> Jasa Bongkaran</a></li>
+
+                            <li><a href="#" class="dropdown-item menu-hover p-3 border-bottom"><i class="bi bi-arrow-right-short"></i> Jasa Renovasi Rumah</a></li>
+
+                            <li><a href="#" class="dropdown-item menu-hover p-3 border-bottom"><i class="bi bi-arrow-right-short"></i> Jasa Renovasi Kantor</a></li>
+
+                            <li><a href="#" class="dropdown-item menu-hover p-3 border-bottom"><i class="bi bi-arrow-right-short"></i> Jasa Renovasi Gudang</a></li>
+
+                            <li><a href="#" class="dropdown-item menu-hover p-3 border-bottom"><i class="bi bi-arrow-right-short"></i> Jasa Renovasi Ruko</a></li>
+
+                            <li><a class="dropdown-item menu-hover p-3" href="#"><i class="bi bi-arrow-right-short"></i> Kontraktor Kolam Renang</a></li>
+                          </ul>
+                    </li>
+                    <hr>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link menu-warning"><i class="bi bi-record"></i> Blog</a>
+                    </li>
+                    <hr>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link menu-warning"><i class="bi bi-record"></i> Karir</a>
+                    </li>
+                    <hr>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link menu-warning"><i class="bi bi-record"></i> Kontak Kami</a>
+                    </li>
+                    <hr>
+                    <li class="nav-item">
+                        <a class="nav-link dropdown-toggle menu-warning" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-record"></i> Portofolio 
+                        </a>
+                          <ul class="dropdown-menu">
+                            <li><a class="dropdown-item p-3 menu-hover border-bottom" href="#"><i class="bi bi-arrow-right-short"></i> Projek</a></li>
+                            <li><a class="dropdown-item p-3 menu-hover" href="#"><i class="bi bi-arrow-right-short"></i> Testimoni</a></li>
+                          </ul>
+                    </li>
+                    <hr>
+                    <li class="nav-item">
+                        <a href="{{ route('login') }}" class="nav-link menu-warning"><i class="bi bi-person"></i> Login</a>
+                    </li>
+                    <hr>
+                    <li class="nav-item">
+                        <a href="{{ route('register') }}" class="nav-link menu-warning"><i class="bi bi-box-arrow-in-right"></i> Register</a>
+                    </li>
+                    <hr>
                 </ul>
                 @else
                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                     <li class="nav-item">
-                        <a href="{{ route('dashboard') }}" class="nav-link text-white"><i class="bi bi-person"></i> Dashboard</a>
+                        <a href="{{ route('dashboard') }}" class="nav-link menu-warning"><i class="bi bi-person"></i> Dashboard</a>
                     </li>
                     <hr>
                     {{-- <li class="nav-item">
